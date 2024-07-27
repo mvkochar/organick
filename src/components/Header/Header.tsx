f07f9a2ce9f6796b5fe747dae7f980bcf0a154c8
sgkochar <sgkochar@gmail.com>
2024-07-27 13:45:59 +0300

    edit Header

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -21,7 +21,7 @@ const Header = () => {
           <input type="text" name='searchQuery' />
           <button type="button" className='btn-clear d-b'><img src="/images/search.png" alt="Search" /></button>
         </form>
-        <div className="header-cart d-f align-center">
+        <div className="header-cart d-f jc-sb align-center">
            <div><img src="/images/cart.png" alt="Cart icon" /></div>
            <a href="" className="header-cart-link">Cart (0)</a>
         </div>
